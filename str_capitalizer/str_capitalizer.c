0acf96d9efc7b5ffaf1a29e559d06a09d50d6169
houssam142 <houssam@Shonjmoj.localdomain>
2024-09-28 18:52:47 +0100

    dsafa

--- str_capitalizer/str_capitalizer.c
+++ str_capitalizer/str_capitalizer.c
@@ -3,7 +3,7 @@ int main(int ac, char **av)
 {
 	int i = 0;
 	int j = 1;
-	if (ac > 1)
+	if (ac >= 2)
 	{
 		while (av[j])
 		{
@@ -11,10 +11,14 @@ int main(int ac, char **av)
 			while (av[j][i])
 			{
 				if (av[j][i] >= 'A' && av[j][i] <= 'Z')
-                                {
+				{
 					av[j][i] += 32;
-                                }
-				if (av[j][i + 1] == 32 || av[j][i + 1] == '\t' || av[j][i + 1] == '\0')
+				}
+				if (av[j][0] >= 'a' && av[j][0] <= 'z')
+				{
+					av[j][0] -= 32;
+				}
+				if (av[j][i - 1] == 32 || (av[j][i - 1] >= 9 && av[j][i - 1] <= 15))
 				{
 					if (av[j][i] >= 'a' && av[j][i] <= 'z')
 					{
